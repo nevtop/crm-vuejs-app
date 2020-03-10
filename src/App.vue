26761939029e14d3eec8468ae1f7f82bccd16ee4
rajankit911 <ankit.raj@employwise.com>
2020-03-10 15:04:55 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+  @import './styles/main.css';
+  @import './styles/util.css';
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <top-nav></top-nav>
+  </div>
+</template>
+
+<script>
+import TopNav from './common/TopNav.vue'
+
+export default {
+  data: function () {
+    return {
+    }
+  },
+  components: {
+    'top-nav': TopNav
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
